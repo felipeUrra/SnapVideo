b1e6344d2c973cf59cb61f65c9c15ae845828a47
felipeUrra <felipeurrarivadeneira@gmail.com>
2023-01-15 00:45:50 +0200

    Menu added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,42 @@
 package org.example;
 
-import java.io.File;
+import java.util.Scanner;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        VideoCreator.createVideo();
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        mostarMenu(scanner);
+    }
+
+    public static void mostarMenu(Scanner scanner) {
+        boolean exit = false;
+
+        System.out.println("SnapVideo");
+
+        while (!exit) {
+            System.out.println("1. Create video" + "\n" +
+                    "2. To change code path" + "\n" +
+                    "3. To change resource path" + "\n" +
+                    "4. Salir" + "\n" +
+                    ":");
+
+            switch (scanner.nextInt()) {
+                case 1:
+                    System.out.println("Code path:");
+                    String path = scanner.next();
+                    VideoCreator.setCmdPath(path);
+
+                    System.out.println("Resources path:");
+                    VideoCreator.setResourcePath(scanner.next());
+
+                    VideoCreator.createVideo();
+                    break;
+                case 2:
+                    System.out.println("Code path:");
+                    VideoCreator.setCmdPath(scanner.nextLine());
+                    break;
+            }
+        }
     }
 }
--- src/main/java/org/example/VideoCreator.java
+++ src/main/java/org/example/VideoCreator.java
@@ -1,15 +1,33 @@
 package org.example;
 
-import java.io.File;
 import java.io.IOException;
 
 public class VideoCreator {
+    private static String cmdPath;
+    private static String resourcePath;
+
+    public static String getCmdPath() {
+        return cmdPath;
+    }
+
+    public static void setCmdPath(String cP) {
+        cmdPath = cP;
+    }
+
+    public static String getResourcePath() {
+        return resourcePath;
+    }
+
+    public static void setResourcePath(String rP) {
+        resourcePath = rP;
+    }
 
     public static void createVideo() {
         try {
-            String path = "/home/gemelos/projects/intelliJ/SnapVideo/SnapVideo.sh";
-            String[] cmd = {"sh", path};
+            String[] cmd = {"sh", cmdPath};
             Runtime.getRuntime().exec(cmd);
+
+            System.out.println("Created video");
         }
         catch (IOException ioe) {
             System.out.println(ioe);
